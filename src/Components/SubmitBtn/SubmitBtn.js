75477a5ab301e0d4171aad28ef6a51aa7dfd344d
Junaid Khan <86207985+Junaid1240651@users.noreply.github.com>
2023-08-11 02:27:50 +0530

    Update SubmitBtn.js

--- src/Components/SubmitBtn/SubmitBtn.js
+++ src/Components/SubmitBtn/SubmitBtn.js
@@ -38,7 +38,7 @@ const SubmitBtn = () => {
 
     try {
       axios
-        .post("http://localhost:3000/", {
+        .post("https://super-assistant-assignment.onrender.com/", {
           categoryDragAndDropData: categoryDragAndDropData,
           dropDownBlanksData: dropDownBlanksData,
           comprehensionData: comprehensionData,
